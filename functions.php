ff9ec1a3a72c47c42cd8a74ba4d9c82b9cc1028e
tarotum <onyskiv.maxim@gmail.com>
2019-12-15 19:08:22 +0200

    initial commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * gb_headless functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package gb_headless
+ */
+
+/**
+ * Enqueue scripts and styles.
+ */
+function gb_headless_scripts() {
+	wp_enqueue_style( 'gb_headless-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'gb_headless_scripts' );
+
