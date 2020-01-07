46a26f68a5891b3330322658c1a668f667bbf703
tarotum <onyskiv.maxim@gmail.com>
2020-01-07 21:39:57 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 function gb_headless_thumbnails() {
     add_theme_support( 'post-thumbnails' );
 }
-add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
+add_action( 'after_setup_theme', 'gb_headless_thumbnails' );
 
 /**
  * Enqueue scripts and styles.
